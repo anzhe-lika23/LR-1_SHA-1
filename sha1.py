31f706e10f430f49b6c92e8f47d7d0104440c2e9
Anzhelika <142719544+anzhe-lika23@users.noreply.github.com>
2023-09-22 10:43:05 +0300

    Corrected the names of changes

--- sha1.py
+++ sha1.py
@@ -1,71 +1,74 @@
 def sha1(data):
-    bytes = ""
+    bytes_string = ""
 
-    h0 = 0x67452301
-    h1 = 0xEFCDAB89
-    h2 = 0x98BADCFE
-    h3 = 0x10325476
-    h4 = 0xC3D2E1F0
+    const_a = 0x67452301
+    const_b = 0xEFCDAB89
+    const_c = 0x98BADCFE
+    const_d = 0x10325476
+    const_e = 0xC3D2E1F0
 
-    for n in range(len(data)):
-        bytes+='{0:08b}'.format(ord(data[n]))
-    bits = bytes+"1"
-    pBits = bits
-    #pad until length equals 448 mod 512
-    while len(pBits)%512 != 448:
-        pBits+="0"
-    #append the original length
-    pBits+='{0:064b}'.format(len(bits)-1)
+    for letter in range(len(data)):
+        bytes_string += '{0:08b}'.format(ord(data[letter]))
+    bits = bytes_string + "1"
+    origin_bits = bits
+    # added "0" until length equals 448 mod 512
+    while len(origin_bits) % 512 != 448:
+        origin_bits += "0"
+    # adding a 64-bit binary number representing the length of the input data
+    origin_bits += '{0:064b}'.format(len(bits) - 1)
 
-    def chunks(l, n):
-        return [l[i:i+n] for i in range(0, len(l), n)]
+    def line_break(line, substring_length):
+        return [line[element:element + substring_length] for element in range(0, len(line), substring_length)]
 
-    def rol(n, b):
-        return ((n << b) | (n >> (32 - b))) & 0xffffffff
+    def cyclic_bit_shift(number, shift):
+        return ((number << shift) | (number >> (32 - shift))) & 0xffffffff
 
-    for c in chunks(pBits, 512):
-        words = chunks(c, 32)
-        w = [0]*80
-        for n in range(0, 16):
-            w[n] = int(words[n], 2)
+    for c in line_break(origin_bits, 512):
+        words = line_break(c, 32)
+        list_worlds = [0] * 80
+        for letter in range(0, 16):
+            list_worlds[letter] = int(words[letter], 2)
         for i in range(16, 80):
-            w[i] = rol((w[i-3] ^ w[i-8] ^ w[i-14] ^ w[i-16]), 1)
+            list_worlds[i] = cyclic_bit_shift((list_worlds[i - 3] ^ list_worlds[i - 8] ^
+                                               list_worlds[i - 14] ^ list_worlds[i - 16]), 1)
 
-        a = h0
-        b = h1
-        c = h2
-        d = h3
-        e = h4
+        a = const_a
+        b = const_b
+        c = const_c
+        d = const_d
+        e = const_e
 
-        #Main loop
+        # Main loop for calculating the hash value
         for i in range(0, 80):
+            function_value = 0
+            constant = 0
             if 0 <= i <= 19:
-                f = (b & c) | ((~b) & d)
-                k = 0x5A827999
+                function_value = (b & c) | ((~b) & d)
+                constant = 0x5A827999
             elif 20 <= i <= 39:
-                f = b ^ c ^ d
-                k = 0x6ED9EBA1
+                function_value = b ^ c ^ d
+                constant = 0x6ED9EBA1
             elif 40 <= i <= 59:
-                f = (b & c) | (b & d) | (c & d)
-                k = 0x8F1BBCDC
+                function_value = (b & c) | (b & d) | (c & d)
+                constant = 0x8F1BBCDC
             elif 60 <= i <= 79:
-                f = b ^ c ^ d
-                k = 0xCA62C1D6
+                function_value = b ^ c ^ d
+                constant = 0xCA62C1D6
 
-            temp = rol(a, 5) + f + e + k + w[i] & 0xffffffff
+            temp = cyclic_bit_shift(a, 5) + function_value + e + constant + list_worlds[i] & 0xffffffff
             e = d
             d = c
-            c = rol(b, 30)
+            c = cyclic_bit_shift(b, 30)
             b = a
             a = temp
 
-        h0 = h0 + a & 0xffffffff
-        h1 = h1 + b & 0xffffffff
-        h2 = h2 + c & 0xffffffff
-        h3 = h3 + d & 0xffffffff
-        h4 = h4 + e & 0xffffffff
+        const_a = const_a + a & 0xffffffff
+        const_b = const_b + b & 0xffffffff
+        const_c = const_c + c & 0xffffffff
+        const_d = const_d + d & 0xffffffff
+        const_e = const_e + e & 0xffffffff
 
-    return '%08x%08x%08x%08x%08x' % (h0, h1, h2, h3, h4)
+    return '%08x%08x%08x%08x%08x' % (const_a, const_b, const_c, const_d, const_e)
 
 
 print(sha1("hello world"))
